9924b45b11fba9f5b128d22ba1a4f2ef3f694e69
Yoann Couillec <yoanncouillec@gmail.com>
2017-06-15 19:44:03 +0200

    Add populate env

--- src/lispwebeval.ml
+++ src/lispwebeval.ml
@@ -7,12 +7,22 @@ let rec string_of_channel channel accu =
   with
   | End_of_file -> accu
 
+let parse env e = 
+  (eval env (Parser.start Lexer.token (Lexing.from_string e)))
+
+let rec populate_env env = function
+  | (id, e)::rest -> populate_env (extend env id (parse env e)) rest
+  | [] -> env
+
 let _ = 
   let lexbuf = Lexing.from_string (string_of_channel stdin "") in
   try
     let expression = Parser.start Lexer.token lexbuf in
     let env = [] in
-    let env = List.fold_left (fun env tag -> extend env tag (VClosure ("l"^tag, ETag (EString tag, [EIdent ("l"^tag)]), env))) env ["head";"body";"div";"span";"form";"input"] in
+    let tags = ["head";"body";"div";"span";"form";"input"] in
+    let env = List.fold_left (fun env tag -> extend env tag (parse env ("(lambda (l"^tag^") (tag \""^tag^"\" l"^tag^"))"))) env tags in
+    let env = populate_env env [("id","(lambda (x) x)");
+				("idd","(lambda (x) (id x))")] in
     let value = eval env expression in
     print_string (string_of_value value) ; print_newline ()
   with 
