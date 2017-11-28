0b8c04f5430056222da3b3e746c68dce138ce7f0
Yoann <yoanncouillec@gmail.com>
2017-11-28 22:31:49 +0100

    string length

--- src/denotationel/host.ml
+++ src/denotationel/host.ml
@@ -1,44 +1,64 @@
 open Value
 
-let flag_of_string_value = function
-  | VString "O_CREAT" -> Unix.O_CREAT
-  | VString "O_RDWR" -> Unix.O_RDWR
-  | _ -> failwith "flag_of_string_value: not managed"
-
-let unix_openfile = function
-  | VList(VString(name)::VList(flags)::VString(perm)::[]) -> 
-     (VFile (Unix.openfile
-	       name 
-	       (List.map flag_of_string_value flags)
-	       (int_of_string perm)))
-  | _ -> failwith "unix_openfile"
-
-let unix_close = function
-  | VList(VFile(fd)::[]) -> 
-     Unix.close fd;
-     VUnit
-  | _ -> failwith "unix_close"
+module Unix = struct
+  
+  let flag_of_string_value = function
+    | VString "O_CREAT" -> Unix.O_CREAT
+    | VString "O_RDWR" -> Unix.O_RDWR
+    | _ -> failwith "flag_of_string_value: not managed"
+		    
+  let unix_openfile = function
+    | VList(VString(name)::VList(flags)::VString(perm)::[]) -> 
+       (VFile (Unix.openfile
+		 name 
+		 (List.map flag_of_string_value flags)
+		 (int_of_string perm)))
+    | _ -> failwith "unix_openfile"
+		    
+  let unix_close = function
+    | VList(VFile(fd)::[]) -> 
+       Unix.close fd;
+       VUnit
+    | _ -> failwith "unix_close"
+		    
+  let unix_read = function
+    | VList(VFile(fd)::VString(buff)::VInt(ofs)::VInt(len)::[]) -> 
+       VInt(Unix.read fd buff ofs len)
+    | _ -> failwith "unix_read"
+		    
+  let unix_write = function
+    | VList(VFile(fd)::VString(buff)::VInt(ofs)::VInt(len)::[]) -> 
+       VInt(Unix.write fd buff ofs len)
+    | _ -> failwith "unix_write"
+		    
+  let unix_stdout = function
+    | VList([]) -> 
+       VFile(Unix.stdout)
+    | _ -> failwith "unix_stdout"
+		    
+end
+		
+module String = struct
+  
+  let string_make = function
+    |  VList(VInt(n)::VChar(c)::[]) ->
+	VString(String.make n c)
+    | _ -> failwith "string_make"		    
+		    
+  let string_length = function
+    |  VList(VString(s)::[]) ->
+	VInt(String.length s)
+    | _ -> failwith "string_length"		    
+		    
+end
 		  
-let string_make = function
-  |  VList(VInt(n)::VChar(c)::[]) ->
-      VString(String.make n c)
-  | _ -> failwith "string_make"
-		  
-let unix_read = function
-  | VList(VFile(fd)::VString(buff)::VInt(ofs)::VInt(len)::[]) -> 
-     VInt(Unix.read fd buff ofs len)
-  | _ -> failwith "unix_read"
-
-let unix_write = function
-  | VList(VFile(fd)::VString(buff)::VInt(ofs)::VInt(len)::[]) -> 
-     VInt(Unix.write fd buff ofs len)
-  | _ -> failwith "unix_write"
-
 let functions = 
   [
-    ("Unix.openfile", unix_openfile);
-    ("Unix.close", unix_close);
-    ("Unix.read", unix_read);
-    ("Unix.write", unix_write);
-    ("String.make", string_make)
+    ("Unix.openfile", Unix.unix_openfile);
+    ("Unix.close", Unix.unix_close);
+    ("Unix.read", Unix.unix_read);
+    ("Unix.write", Unix.unix_write);
+    ("Unix.stdout", Unix.unix_stdout);
+    ("String.make", String.string_make);
+    ("String.length", String.string_length)
   ]
--- src/denotationel/test/host.scm
+++ src/denotationel/test/host.scm
@@ -1,5 +1,7 @@
 (let (fd (hostcall Unix.openfile (list "tmp.tmp" (list "O_CREAT" "O_RDWR") "0o640")))
   (let (buff "1234567890")
-    (begin
-      (hostcall Unix.write (list fd buff 0 10))
-      (hostcall Unix.close (list fd)))))
+    (let (len (hostcall String.length (list buff)))
+      (begin
+	(hostcall Unix.write (list fd buff 0 len))
+	(hostcall Unix.write (list (hostcall Unix.stdout (list)) buff 0 len))
+	(hostcall Unix.close (list fd))))))
