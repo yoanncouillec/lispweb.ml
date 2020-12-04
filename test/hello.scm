4c193c4e5de42a6b36ef3a0d9a529ba4e9f31ebd
Yoann <yoanncouillec@gmail.com>
2020-12-04 21:22:06 +0000

    add hello remote lib

--- test/hello.scm
+++ test/hello.scm
@@ -1,3 +1,3 @@
-(load "lib/hello.scm")
-
+(load "lib/load.scm")
+(load-url "https" "raw.githubusercontent.com" 443 "/yoanncouillec/lispweb.ml/master/lib/hello.scm")
 (hello)
--- test/testapp.scm
+++ test/testapp.scm
@@ -1,6 +1,5 @@
 (load "lib/load.scm")
 (load-url "https" "raw.githubusercontent.com" 443 "/yoanncouillec/lispweb.ml/master/lib/assert.scm")
-(print "loaded")
 
 (assert
  :expects 12
