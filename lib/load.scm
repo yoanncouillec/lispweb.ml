87bb684802647612db25dff4e89b84953a7fb9f4
Yoann <yoanncouillec@gmail.com>
2020-12-10 17:17:26 +0000

    load with cache

--- lib/load.scm
+++ lib/load.scm
@@ -7,18 +7,22 @@
 
 (define load-file
   (lambda (filename
-	   :handle-no-such-file (lambda () #f)
+	   :handle-sys-error (lambda () #f)
 	   :handle-wrong-arguments (lambda () #f))
     (load-string
      (input-lines
-      (open-in filename)))))
+      (open-in filename
+	       :handle-sys-error handle-sys-error)))))
 
 (define load-url
   (lambda (scheme host port path file)
     (load-string (wget scheme host port (concat "" (list path file))))))
 
 (define load-url-with-local-cache
-  (lambda (scheme host port path lib :cache-path "./")
-    (load-file lib)))
+  (lambda (scheme host port path file :cache-path "./")
+    (catch sys-error
+	   (load-file file
+		      :handle-sys-error
+		      (lambda () (load-url scheme host port path file))))))
 
   
