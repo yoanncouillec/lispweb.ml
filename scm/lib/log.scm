b7a85b48c13e38c737e68f9ccda560a2dfebfbb6
Yoann <yoann@flowlity.com>
2022-03-24 20:37:31 +0000

    create logger

--- scm/lib/log.scm
+++ scm/lib/log.scm
@@ -2,6 +2,33 @@
 (import "ansiterminal.scm")
 (import "process.scm")
 
+(define logger
+  (lambda (min-level)
+    (lambda (level msg)
+      (cond
+       ((equal? min-level "DEBUG")
+	(begin
+	  (if (equal? level "DEBUG") (debug msg)
+	      (if (equal? level "INFO") (info msg)	      
+		  (if (equal? level "WARNING") (warning msg)
+		      (if (equal? level  "ERROR") (error msg)
+			  (if (equal? level "FATAL") (fatal msg))))))))
+       ((equal? min-level "INFO")
+	(begin
+	  (if (equal? level "INFO") (info msg)
+	      (if (equal? level "WARNING") (warning msg)
+		  (if (equal? level  "ERROR") (error msg)
+		      (if (equal? level "FATAL") (fatal msg)))))))
+       ((equal? min-level "WARNING")
+	(if (equal? level "WARNING") (warning msg)
+	    (if (equal? level  "ERROR") (error msg)
+		(if (equal? level "FATAL") (fatal msg)))))
+       ((equal? min-level  "ERROR")
+	(if (equal? level  "ERROR") (error msg)
+	    (if (equal? level "FATAL") (fatal msg))))
+       ((equal? min-level "FATAL")
+	(if (equal? level "FATAL") (fatal msg)))))))
+
 (define log
   (lambda (level spaces ss s msg)
     (let ((style-print (if (or (equal? level "ERROR") (equal? level "FATAL")) style-prerr-string style-print-string))
@@ -19,13 +46,10 @@
       (style-print s msg)
       (regular-print-nl))))
 
-
-
 (define debug (lambda (msg) (log "DEBUG" "   " (list (style "bold") (style "white") (style "on-green")) (list (style "green") (style "bold")) msg)))
 (define info (lambda (msg) (log "INFO" "    " (list (style "bold")) (list) msg)))
 (define warning (lambda (msg) (log "WARNING" " " (list (style "blue") (style "bold")) (list (style "blue") (style "bold")) msg)))
-(define success (lambda (msg) (log "SUCCESS" " " (list (style "green") (style "bold")) (list (style "green")) msg)))
 (define error (lambda (msg) (log "ERROR" "   " (list (style "white") (style "on-red") (style "bold")) (list (style "red")) msg)))
 (define fatal (lambda (msg) (log "FATAL" "   " (list (style "white") (style "on-red") (style "bold")) (list (style "red") (style "bold")) msg)))
 
-
+(define success (lambda (msg) (log "SUCCESS" " " (list (style "green") (style "bold")) (list (style "green")) msg)))
--- scm/test/hello.scm
+++ scm/test/hello.scm
@@ -1,13 +1,14 @@
 (import "standard.scm")
 (import "process.scm")
 
+(define mylogger (logger --level))
+
 (print (gethostname))
-(debug "loading library")
-(info "loading library")
+(mylogger "DEBUG" "loading library")
+(mylogger "INFO" "loading library")
 (load-string (wget "https" "raw.githubusercontent.com" 443 "/yoanncouillec/lispweb.ml/master/scm/lib/hello.scm"))
-(success "library loaded")
 (print (hello :name "to Lispw"))
-(error "this is to test error log")
-(fatal "this is to test fatal log")
-(warning "this is to test warning log")
+(mylogger "ERROR" "this is to test error log")
+(mylogger "FATAL" "this is to test fatal log")
+(mylogger "WARNING" "this is to test warning log")
 
